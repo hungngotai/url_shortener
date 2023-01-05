1acbc01c90b9315aae3e8e5556bd6e99c75d695c
Ngo Tai Hung <ngotaihung@gmail.com>
2023-01-05 23:16:43 +0700

    Prevent collision with counter

--- app/services/url_shortener_service.rb
+++ app/services/url_shortener_service.rb
@@ -1,4 +1,7 @@
 class UrlShortenerService
+  LIMIT_LENGTH = 7
+  BASE_62_STRING = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.freeze
+
   attr_reader :original_url
 
   def self.call(original_url)
@@ -11,8 +14,29 @@ class UrlShortenerService
 
   def call
     unescape_url = CGI.unescape(original_url)
-    Url.find_or_create_by(original_url: unescape_url) do |url|
-      url.shortened = SecureRandom.hex.first(7)
-    end.shortened
+    url_with_counter = "#{unescape_url}counter=#{counter}"
+    url_md5_digest = Digest::MD5.hexdigest(url_with_counter)
+    shortened = to_base62(url_md5_digest.to_i(16)).first(LIMIT_LENGTH)
+    url = Url.new(original_url: url_with_counter, shortened: 'shortened')
+    url.save
+    url
+  end
+
+  private
+
+  def counter
+    cache = Rails.cache
+    counter = cache.read(:counter).to_i
+    cache.write(:counter, counter + 1)
+    counter.to_s
+  end
+
+  def to_base62(decimal)
+    hash_str = ''
+    while decimal.positive?
+      hash_str = BASE_62_STRING[decimal % 62] + hash_str
+      decimal /= 62
+    end
+    hash_str
   end
 end
