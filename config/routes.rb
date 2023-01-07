d3b77558db5fb4a004dc8350f7dadce9ef941f23
Ngo Tai Hung <ngotaihung@gmail.com>
2023-01-07 12:08:49 +0700

    Change endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@
 Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post '/encoded', to: 'url#encoded'
-  post '/decoded', to: 'url#decoded'
+  post '/encode', to: 'url#encode'
+  post '/decode', to: 'url#decode'
 end
